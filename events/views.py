36d54b8507fd9eaefd2eb23a7e888121c84a885c
Jaime Viñas <jaimevp54@gmail.com>
2017-07-19 14:18:14 -0400

    Add date and distance filters (#1)
    * Added search button with modal to the navbar
    [changed] reduced the top and bottom padding on the navbar
    
    * Added fullcalendar to search modal
    
    * Added calendar and distance filter

--- events/views.py
+++ events/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from .models import RacePage
+from django.http import JsonResponse
+
+from datetime import datetime
+
 
 # Create your views here.
+
+def filter_events(request):
+    if request.method == "GET":
+
+        races = RacePage.objects.filter(
+            date__gt=datetime.fromtimestamp(int(request.GET["start"])),
+            date__lt=datetime.fromtimestamp(int(request.GET["end"])),
+            distance__gt=int(request.GET["distance_min"]),
+            distance__lt=int(request.GET["distance_max"]),
+        ).values('title', 'date', 'url_path')
+
+        events = []
+        for race in list(races):
+            race['start'] = race['date'].isoformat()
+            race['className'] = "success"
+            race['url'] = race['url_path'][9:]  # slices to remove '/carrera' # TODO make this work for all cases
+            events.append(race)
+
+    return JsonResponse(events, safe=False)
