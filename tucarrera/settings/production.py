3dc0d26c2ceab944c7802e791174c4cad8874bb8
jaimevp54 <jaimevp54@gmail.com>
2017-08-11 06:44:10 -0400

    Upadated production settings file

--- tucarrera/settings/production.py
+++ tucarrera/settings/production.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 
+from decouple import config
 from .base import *
 
 DEBUG = False
+SECRET_KEY = config("SECRET_KEY")
+ALLOWED_HOSTS = ['*']
 
 try:
     from .local import *
